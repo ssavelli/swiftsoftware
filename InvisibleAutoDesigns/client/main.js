6627fc5d1f740784deaa4ee442263c33bed01d9c
ssavelli <ssavelli@visionwaresolutions.com>
2016-02-18 17:06:34 -0500

    Added base side

---
+++ InvisibleAutoDesigns/client/main.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+const ReactDOM = require('react-dom');
+const React = require('react');
+
+require('./css/bootstrap.min.css');
+require('./css/main.css');
+
+require('./js/bootstrap.min.js');
+
---
+++ InvisibleAutoDesigns/webpack.config.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './client/main.js',
+  output: { path: __dirname, filename: './client/bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: /client/
+      },
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      { test: /\.(woff|woff2)$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf$/, loader: "file-loader" },
+      { test: /\.eot$/, loader: "file-loader" },
+      { test: /\.svg$/, loader: "file-loader" }
+    ]
+  }
+};
